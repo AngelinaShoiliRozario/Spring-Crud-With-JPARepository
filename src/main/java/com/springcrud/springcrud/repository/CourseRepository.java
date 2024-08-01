564fef20e2249786c209ac6d191d774f78125417
Angelina_Shoili_Rozario <shoili.rozario@gmail.com>
2024-08-01 01:16:33 -0600

    custom security

--- src/main/java/com/springcrud/springcrud/repository/CourseRepository.java
+++ src/main/java/com/springcrud/springcrud/repository/CourseRepository.java
@@ -2,8 +2,10 @@ package com.springcrud.springcrud.repository;
 
 import com.springcrud.springcrud.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource(path = "topics")
 public interface CourseRepository extends JpaRepository<Course, Long> {
 }
